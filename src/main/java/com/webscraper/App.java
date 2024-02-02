ba027a5d78372e9bacb2fe2927649f95385b3a74
penszzip <pazhiloyflex@gmail.com>
2024-02-01 23:13:55 -0500

    formatted output

--- src/main/java/com/webscraper/App.java
+++ src/main/java/com/webscraper/App.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import java.util.List;
-import java.time.Duration;
 
 public class App 
 {
@@ -14,17 +13,34 @@ public class App
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.skysports.com/tennis/scores-schedule/01-02-2024");
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-//        WebElement element = driver.findElement(By.className("ui-tournament-matches"));
-//        System.out.println(element.getText());
+
         List<WebElement> matches = driver.findElements(By.className("ui-tennis-match-score__table"));
+
+        String template3 = "%s vs %s, %s\n";
+        String template9 = "%s vs %s, %s - %s, %s - %s, %s\n";
+        String template11 = "%s, %s vs %s, %s, %s - %s, %s - %s, %s\n";
+        String template12 = "%s vs %s, %s - %s, %s - %s, %s - %s, %s\n";
+        String template14 = "%s, %s vs %s, %s, %s - %s, %s - %s, %s - %s, %s\n";
+
         for (WebElement match : matches) {
-            System.out.println(match.getText());
-//            String[] lines = match.getText().split("\n");
-//            if (lines.length >= 4) {
-//                System.out.println(lines[0] + " vs " + lines[1] + ", Score: " + lines[2] + ", Winner: " + lines[3]);
-//            }
+            String[] lines = match.getText().split("\n");
+            if (lines.length == 3) {
+                System.out.println(String.format(template3, lines[0], lines[1], lines[2]));
+            }
+            else if (lines.length == 9) {
+                System.out.println(String.format(template9, lines[0], lines[1], lines[2], lines[4], lines[5], lines[7], lines[8]));
+            }
+            else if (lines.length == 11) {
+                System.out.println(String.format(template11, lines[0], lines[1], lines[2], lines[3], lines[4], lines[6], lines[7], lines[9], lines[10]));
+            }
+            else if (lines.length == 12) {
+                System.out.println(String.format(template12, lines[0], lines[1], lines[2], lines[4], lines[5], lines[7], lines[8], lines[10], lines[11]));
+            }
+            else if (lines.length == 14) {
+                System.out.println(String.format(template14, lines[0], lines[1], lines[2], lines[3], lines[4], lines[6], lines[7], lines[9], lines[10], lines[12], lines[13]));
+            }
         }
+
         driver.quit();
     }
 }
